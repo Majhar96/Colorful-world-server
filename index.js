c37f33c2e2b4d1e9792b911cf207aaa85063b181
Majhar96 <majharul.islam.17321@gmail.com>
2021-11-16 00:14:17 +0600

    commit

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const port = process.env.PORT || 5000;
 
 
 app.use(cors());
+app.use(express.json());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.y1i3r.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
@@ -19,7 +20,8 @@ async function run() {
     try {
         await client.connect();
         const database = client.db('sunglassDb');
-        const productsCollection = database.collection('products')
+        const productsCollection = database.collection('products');
+        const usersCollection = database.collection('users');
 
 
         // Get API
@@ -59,6 +61,42 @@ async function run() {
             const result = await productsCollection.deleteOne(query);
             res.json(result);
         })
+
+        app.get('/users/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email: email };
+            const user = await usersCollection.findOne(query);
+            let isAdmin = false;
+            if (user?.role === 'admin') {
+                isAdmin = true;
+            }
+            res.json({ admin: isAdmin });
+        })
+
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const result = await usersCollection.insertOne(user);
+            console.log(result);
+            res.json(result);
+        });
+
+        // app.put('/users/admin', verifyToken, async (req, res) => {
+        //     const user = req.body;
+        //     const requester = req.decodedEmail;
+        //     if (requester) {
+        //         const requesterAccount = await usersCollection.findOne({ email: requester });
+        //         if (requesterAccount.role === 'admin') {
+        //             const filter = { email: user.email };
+        //             const updateDoc = { $set: { role: 'admin' } };
+        //             const result = await usersCollection.updateOne(filter, updateDoc);
+        //             res.json(result);
+        //         }
+        //     }
+        //     else {
+        //         res.status(403).json({ message: 'Sorry not have access to make admin' })
+        //     }
+
+        // })
 
 
     } finally {
